96c579e0e7a38186df94a46ffeac9832ad68a342
Thomas Kavanagh <tomokav@live.ie>
2015-10-12 18:19:07 +0100

    Skeleton User profile page support

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
 
+
+          resources :users          # NEW LINE
+    
+
           root 'static_pages#home'
 
           get 'help'    => 'static_pages#help'
